19124acc54767791edbbcbf97c46d86482af392d
hcnguyen5 <hcnguyen5@users.noreply.github.com>
2024-03-11 15:48:40 -0500

    Make Iterator/Iterable methods accessible to JS

--- jre/java/java/util/Iterator.java
+++ jre/java/java/util/Iterator.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,6 +18,7 @@ package java.util;
 import static javaemul.internal.InternalPreconditions.checkNotNull;
 
 import java.util.function.Consumer;
+import jsinterop.annotations.JsMethod;
 
 /**
  * See <a href="https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html">the official Java
@@ -27,8 +28,10 @@ import java.util.function.Consumer;
  */
 public interface Iterator<T> {
 
+  @JsMethod
   boolean hasNext();
 
+  @JsMethod
   T next();
 
   default void forEachRemaining(Consumer<? super T> consumer) {
